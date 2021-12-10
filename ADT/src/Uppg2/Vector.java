f9c03019e91a6343e19a397acad844a20b8eaecf
fredrikstrandberg <fredrik.strandberg@live.se>
2021-12-10 13:41:23 +0100

    Uppg1 klar

---
+++ ADT/src/Uppg2/Vector.java
@@ -0,0 +1,61 @@
+package Uppg2;
+
+import java.util.List;
+
+class Vector <E extends Comparable<E>> implements SparseVec<E>{
+
+    @Override
+    public void add(E elem) {
+
+    }
+
+    @Override
+    public void add(int pos, E elem) {
+
+    }
+
+    @Override
+    public int indexOf(E elem) {
+        return 0;
+    }
+
+    @Override
+    public void removeAt(int pos) {
+
+    }
+
+    @Override
+    public void removeElem(E elem) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int minIndex() {
+        return 0;
+    }
+
+    @Override
+    public int maxIndex() {
+        return 0;
+    }
+
+    @Override
+    public E get(int pos) {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public List<E> sortedValues() {
+        return null;
+    }
+}
