8eab1509f544c7537a968661f4cd577f22a372bf
fredrikstrandberg <fredrik.strandberg@live.se>
2021-12-10 10:21:48 +0100

    commit

--- ADT/src/NumberedItem.java
+++ ADT/src/NumberedItem.java
@@ -1,4 +1,19 @@
 
+class NumberedItem<T> {
+    int index;
+    T value;
 
-public class NumberedItem {
+    NumberedItem(int i, T t) {
+        this.index = Math.max(i, 0);
+        this.value = t;
+    }
+
+    public int compareTo(int i2){
+        return Math.abs(this.index - i2);
+    }
+
+    public String toString() {
+        return "Index: " + String.valueOf(index) + " Value: " + value;
+    }
+
 }
